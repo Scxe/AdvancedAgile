8c3720cf7f9f06578f6da731387e99a02b41455a
Scxe Scxe <ajt2800@yahoo.com>
2019-09-25 15:51:00 -0500

    Try2

--- Group_Project1/TinyCollege/frmStudentAdd.Designer.cs
+++ Group_Project1/TinyCollege/frmStudentAdd.Designer.cs
@@ -39,18 +39,20 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(37, 49);
+            this.label1.Location = new System.Drawing.Point(56, 75);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.Size = new System.Drawing.Size(148, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Enter Student Data";
             // 
             // btnAddStudent
             // 
             this.btnAddStudent.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAddStudent.Location = new System.Drawing.Point(71, 229);
+            this.btnAddStudent.Location = new System.Drawing.Point(106, 352);
+            this.btnAddStudent.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnAddStudent.Name = "btnAddStudent";
-            this.btnAddStudent.Size = new System.Drawing.Size(127, 64);
+            this.btnAddStudent.Size = new System.Drawing.Size(190, 98);
             this.btnAddStudent.TabIndex = 1;
             this.btnAddStudent.Text = "Add Student";
             this.btnAddStudent.UseVisualStyleBackColor = true;
@@ -59,9 +61,10 @@
             // btnClose
             // 
             this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClose.Location = new System.Drawing.Point(286, 229);
+            this.btnClose.Location = new System.Drawing.Point(429, 352);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(127, 64);
+            this.btnClose.Size = new System.Drawing.Size(190, 98);
             this.btnClose.TabIndex = 2;
             this.btnClose.Text = "Close";
             this.btnClose.UseVisualStyleBackColor = true;
@@ -71,42 +74,48 @@
             // 
             this.lblNewStudentName.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.lblNewStudentName.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNewStudentName.Location = new System.Drawing.Point(66, 93);
+            this.lblNewStudentName.Location = new System.Drawing.Point(99, 143);
+            this.lblNewStudentName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNewStudentName.Name = "lblNewStudentName";
-            this.lblNewStudentName.Size = new System.Drawing.Size(94, 88);
+            this.lblNewStudentName.Size = new System.Drawing.Size(141, 135);
             this.lblNewStudentName.TabIndex = 3;
             this.lblNewStudentName.Text = "New Student Name:";
             this.lblNewStudentName.Click += new System.EventHandler(this.LblNewStudentName_Click);
             // 
             // txtStudentName
             // 
-            this.txtStudentName.Location = new System.Drawing.Point(261, 150);
+            this.txtStudentName.Location = new System.Drawing.Point(392, 231);
+            this.txtStudentName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtStudentName.Name = "txtStudentName";
-            this.txtStudentName.Size = new System.Drawing.Size(152, 20);
+            this.txtStudentName.Size = new System.Drawing.Size(226, 26);
             this.txtStudentName.TabIndex = 4;
             // 
             // lstTestBox
             // 
             this.lstTestBox.FormattingEnabled = true;
-            this.lstTestBox.Location = new System.Drawing.Point(261, 35);
+            this.lstTestBox.ItemHeight = 20;
+            this.lstTestBox.Location = new System.Drawing.Point(392, 54);
+            this.lstTestBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.lstTestBox.Name = "lstTestBox";
-            this.lstTestBox.Size = new System.Drawing.Size(152, 95);
+            this.lstTestBox.Size = new System.Drawing.Size(226, 144);
             this.lstTestBox.TabIndex = 5;
             this.lstTestBox.SelectedIndexChanged += new System.EventHandler(this.LstTestBox_SelectedIndexChanged);
             // 
             // frmStudentAdd
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(454, 330);
+            this.ClientSize = new System.Drawing.Size(681, 508);
             this.Controls.Add(this.lstTestBox);
             this.Controls.Add(this.txtStudentName);
             this.Controls.Add(this.lblNewStudentName);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.btnAddStudent);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frmStudentAdd";
             this.Text = "Add a Student";
+            this.Load += new System.EventHandler(this.FrmStudentAdd_Load_1);
             this.ResumeLayout(false);
             this.PerformLayout();
 
