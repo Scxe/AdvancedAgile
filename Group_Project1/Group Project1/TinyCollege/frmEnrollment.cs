58f09c2d0dc14bdd2274e97013b24510d51905a9
jdjoers1 <55814661+jdjoers1@users.noreply.github.com>
2019-09-25 18:38:37 -0500

    added for the 4th form

---
+++ Group_Project1/Group Project1/TinyCollege/frmEnrollment.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Web;
+using System.Configuration;
+
+namespace TinyCollege
+{
+    public partial class frmEnrollment : Form
+    {
+        SqlConnection connection;
+        string connectionString;
+        public frmEnrollment()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["TinyCollege.Properties.Settings.StudentDBConnectionString"].ConnectionString;
+            fillStudentComboBox();
+            fillCoursesComboBox();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            new frmMain().Show();
+            this.Close();
+        }
+
+        private void fillStudentComboBox()
+        {
+            string query = "SELECT * FROM studentDB";
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader myreader = command.ExecuteReader();
+                    while (myreader.Read())
+                    {
+                        string sname = myreader.GetString(1);
+                        cmbStudent.Items.Add(sname);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+        }
+
+        private void fillCoursesComboBox()
+        {
+            string query = "SELECT * FROM coursesDB";
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader myreader = command.ExecuteReader();
+                    while (myreader.Read())
+                    {
+                        string sname = myreader.GetString(1);
+                        cmbCourse.Items.Add(sname);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+        }
+
+        private void StudentComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void EnrollButton_Click(object sender, EventArgs e)
+        {
+
+            Object cTitle = cmbCourse.SelectedItem;
+            Object sName = cmbStudent.SelectedItem;
+            string query = "INSERT INTO enrollmentDB (studentName, coursesTitle) VALUES ('" + sName + "','" + cTitle + "')";
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
+
---
+++ Group_Project1/Group Project1/TinyCollege/frmWhatCoursesAStudentisEnrolledIn.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace TinyCollege
+{
+    partial class frmWhatCoursesAStudentisEnrolledIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(579, 324);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(160, 70);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "Cl&ose";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // frmWhatCoursesAStudentisEnrolledIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClose);
+            this.Name = "frmWhatCoursesAStudentisEnrolledIn";
+            this.Text = "frmWhatCoursesAStudentisEnrolledIn";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClose;
+    }
+}
