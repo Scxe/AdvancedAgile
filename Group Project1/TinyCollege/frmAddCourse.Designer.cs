765dbc6a585efc465723b824c89dd47abc46a1b1
7sluck <56097522+7sluck@users.noreply.github.com>
2019-10-10 21:41:05 -0500

    Update frmAddCourse.Designer.cs
    Changes tab order.

--- Group Project1/TinyCollege/frmAddCourse.Designer.cs
+++ Group Project1/TinyCollege/frmAddCourse.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace TinyCollege
+namespace TinyCollege
 {
     partial class frmAddCourse
     {
@@ -43,10 +43,11 @@
             // btnClose
             // 
             this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClose.Location = new System.Drawing.Point(471, 315);
+            this.btnClose.Location = new System.Drawing.Point(314, 205);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(228, 82);
-            this.btnClose.TabIndex = 0;
+            this.btnClose.Size = new System.Drawing.Size(152, 53);
+            this.btnClose.TabIndex = 3;
             this.btnClose.Text = "Cl&ose";
             this.btnClose.UseVisualStyleBackColor = true;
             this.btnClose.Click += new System.EventHandler(this.Button1_Click);
@@ -56,11 +57,10 @@
             // btnAddCourse
             // 
             this.btnAddCourse.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAddCourse.Location = new System.Drawing.Point(108, 317);
-            this.btnAddCourse.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnAddCourse.Location = new System.Drawing.Point(72, 206);
             this.btnAddCourse.Name = "btnAddCourse";
-            this.btnAddCourse.Size = new System.Drawing.Size(237, 82);
-            this.btnAddCourse.TabIndex = 1;
+            this.btnAddCourse.Size = new System.Drawing.Size(158, 53);
+            this.btnAddCourse.TabIndex = 2;
             this.btnAddCourse.Text = "Add Course";
             this.btnAddCourse.UseVisualStyleBackColor = true;
             this.btnAddCourse.Click += new System.EventHandler(this.BtnAddCourse_Click);
@@ -71,10 +71,9 @@
             // 
             this.lblCourseTitle.AutoSize = true;
             this.lblCourseTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCourseTitle.Location = new System.Drawing.Point(104, 89);
-            this.lblCourseTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCourseTitle.Location = new System.Drawing.Point(69, 58);
             this.lblCourseTitle.Name = "lblCourseTitle";
-            this.lblCourseTitle.Size = new System.Drawing.Size(231, 40);
+            this.lblCourseTitle.Size = new System.Drawing.Size(157, 29);
             this.lblCourseTitle.TabIndex = 2;
             this.lblCourseTitle.Text = "Course Title: ";
             // 
@@ -82,42 +81,41 @@
             // 
             this.lblSemesterOffered.AutoSize = true;
             this.lblSemesterOffered.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSemesterOffered.Location = new System.Drawing.Point(104, 192);
-            this.lblSemesterOffered.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSemesterOffered.Location = new System.Drawing.Point(69, 125);
             this.lblSemesterOffered.Name = "lblSemesterOffered";
-            this.lblSemesterOffered.Size = new System.Drawing.Size(321, 40);
+            this.lblSemesterOffered.Size = new System.Drawing.Size(216, 29);
             this.lblSemesterOffered.TabIndex = 3;
             this.lblSemesterOffered.Text = "Semester Offered: ";
             // 
             // txtSemOffered
             // 
-            this.txtSemOffered.Location = new System.Drawing.Point(471, 192);
-            this.txtSemOffered.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtSemOffered.Location = new System.Drawing.Point(314, 125);
             this.txtSemOffered.Multiline = true;
             this.txtSemOffered.Name = "txtSemOffered";
-            this.txtSemOffered.Size = new System.Drawing.Size(264, 42);
-            this.txtSemOffered.TabIndex = 4;
+            this.txtSemOffered.Size = new System.Drawing.Size(177, 29);
+            this.txtSemOffered.TabIndex = 1;
             this.txtSemOffered.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtSemOffered_KeyPress);
             this.txtSemOffered.MouseLeave += new System.EventHandler(this.TxtSemOffered_MouseLeave);
             this.txtSemOffered.MouseHover += new System.EventHandler(this.TxtSemOffered_MouseHover);
             // 
             // txtCourseTitle
             // 
-            this.txtCourseTitle.Location = new System.Drawing.Point(471, 86);
-            this.txtCourseTitle.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtCourseTitle.Location = new System.Drawing.Point(314, 56);
             this.txtCourseTitle.Multiline = true;
             this.txtCourseTitle.Name = "txtCourseTitle";
-            this.txtCourseTitle.Size = new System.Drawing.Size(264, 46);
-            this.txtCourseTitle.TabIndex = 5;
+            this.txtCourseTitle.Size = new System.Drawing.Size(177, 31);
+            this.txtCourseTitle.TabIndex = 0;
             this.txtCourseTitle.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtCourseTitle_KeyPress);
             this.txtCourseTitle.MouseLeave += new System.EventHandler(this.TxtCourseTitle_MouseLeave);
             this.txtCourseTitle.MouseHover += new System.EventHandler(this.TxtCourseTitle_MouseHover);
             // 
             // groupBox1
             // 
-            this.groupBox1.Location = new System.Drawing.Point(41, 26);
+            this.groupBox1.Location = new System.Drawing.Point(27, 17);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(718, 396);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(479, 257);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Enter Course Data";
@@ -127,22 +125,23 @@
             this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 427);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 270);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 9, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(533, 22);
             this.statusStrip1.TabIndex = 7;
             this.statusStrip1.Text = "statusStrip1";
             // 
             // toolStripStatusLabel1
             // 
             this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 15);
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
             // 
             // frmAddCourse
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 449);
+            this.ClientSize = new System.Drawing.Size(533, 292);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.txtCourseTitle);
             this.Controls.Add(this.txtSemOffered);
@@ -151,6 +150,7 @@
             this.Controls.Add(this.btnAddCourse);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmAddCourse";
             this.Text = "Add a Course";
             this.statusStrip1.ResumeLayout(false);
