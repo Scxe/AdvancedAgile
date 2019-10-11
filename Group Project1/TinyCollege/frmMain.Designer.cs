b80bead5b7bad8fc75651b2ffe92f8526fa5f07d
7sluck <56097522+7sluck@users.noreply.github.com>
2019-10-10 22:33:15 -0500

    Update frmMain.Designer.cs
    Changes tabbing order.

--- Group Project1/TinyCollege/frmMain.Designer.cs
+++ Group Project1/TinyCollege/frmMain.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace TinyCollege
+namespace TinyCollege
 {
     partial class frmMain
     {
@@ -40,10 +40,9 @@
             // btnAddStudent
             // 
             this.btnAddStudent.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAddStudent.Location = new System.Drawing.Point(114, 172);
-            this.btnAddStudent.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnAddStudent.Location = new System.Drawing.Point(76, 112);
             this.btnAddStudent.Name = "btnAddStudent";
-            this.btnAddStudent.Size = new System.Drawing.Size(198, 94);
+            this.btnAddStudent.Size = new System.Drawing.Size(132, 61);
             this.btnAddStudent.TabIndex = 0;
             this.btnAddStudent.Text = "Add a Student";
             this.btnAddStudent.UseVisualStyleBackColor = true;
@@ -52,11 +51,10 @@
             // btnEnroll
             // 
             this.btnEnroll.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEnroll.Location = new System.Drawing.Point(114, 325);
-            this.btnEnroll.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnEnroll.Location = new System.Drawing.Point(76, 211);
             this.btnEnroll.Name = "btnEnroll";
-            this.btnEnroll.Size = new System.Drawing.Size(198, 94);
-            this.btnEnroll.TabIndex = 1;
+            this.btnEnroll.Size = new System.Drawing.Size(132, 61);
+            this.btnEnroll.TabIndex = 2;
             this.btnEnroll.Text = "Enroll a Student in a Course";
             this.btnEnroll.UseVisualStyleBackColor = true;
             this.btnEnroll.Click += new System.EventHandler(this.BtnEnroll_Click);
@@ -64,11 +62,10 @@
             // btnWhatCourses
             // 
             this.btnWhatCourses.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnWhatCourses.Location = new System.Drawing.Point(114, 469);
-            this.btnWhatCourses.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnWhatCourses.Location = new System.Drawing.Point(76, 305);
             this.btnWhatCourses.Name = "btnWhatCourses";
-            this.btnWhatCourses.Size = new System.Drawing.Size(198, 94);
-            this.btnWhatCourses.TabIndex = 2;
+            this.btnWhatCourses.Size = new System.Drawing.Size(132, 61);
+            this.btnWhatCourses.TabIndex = 4;
             this.btnWhatCourses.Text = "What Courses a Student is Enrolled In";
             this.btnWhatCourses.UseVisualStyleBackColor = true;
             this.btnWhatCourses.Click += new System.EventHandler(this.BtnWhatCourses_Click);
@@ -76,11 +73,10 @@
             // btnAddCourses
             // 
             this.btnAddCourses.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAddCourses.Location = new System.Drawing.Point(474, 172);
-            this.btnAddCourses.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnAddCourses.Location = new System.Drawing.Point(316, 112);
             this.btnAddCourses.Name = "btnAddCourses";
-            this.btnAddCourses.Size = new System.Drawing.Size(198, 94);
-            this.btnAddCourses.TabIndex = 3;
+            this.btnAddCourses.Size = new System.Drawing.Size(132, 61);
+            this.btnAddCourses.TabIndex = 1;
             this.btnAddCourses.Text = "Add a Course";
             this.btnAddCourses.UseVisualStyleBackColor = true;
             this.btnAddCourses.Click += new System.EventHandler(this.BtnAddCourses_Click);
@@ -88,11 +84,10 @@
             // btnWho
             // 
             this.btnWho.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnWho.Location = new System.Drawing.Point(474, 325);
-            this.btnWho.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnWho.Location = new System.Drawing.Point(316, 211);
             this.btnWho.Name = "btnWho";
-            this.btnWho.Size = new System.Drawing.Size(198, 94);
-            this.btnWho.TabIndex = 4;
+            this.btnWho.Size = new System.Drawing.Size(132, 61);
+            this.btnWho.TabIndex = 3;
             this.btnWho.Text = "Who is in a Course";
             this.btnWho.UseVisualStyleBackColor = true;
             this.btnWho.Click += new System.EventHandler(this.BtnWho_Click);
@@ -100,10 +95,9 @@
             // btnExit
             // 
             this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnExit.Location = new System.Drawing.Point(474, 469);
-            this.btnExit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnExit.Location = new System.Drawing.Point(316, 305);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(198, 94);
+            this.btnExit.Size = new System.Drawing.Size(132, 61);
             this.btnExit.TabIndex = 5;
             this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = true;
@@ -113,18 +107,17 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(117, 88);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(78, 57);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(552, 37);
+            this.label1.Size = new System.Drawing.Size(376, 26);
             this.label1.TabIndex = 6;
             this.label1.Text = "Tiny College Course Tracking System";
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(786, 692);
+            this.ClientSize = new System.Drawing.Size(524, 450);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnWho);
@@ -132,7 +125,6 @@
             this.Controls.Add(this.btnWhatCourses);
             this.Controls.Add(this.btnEnroll);
             this.Controls.Add(this.btnAddStudent);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frmMain";
             this.Text = "Welcome";
             this.ResumeLayout(false);
