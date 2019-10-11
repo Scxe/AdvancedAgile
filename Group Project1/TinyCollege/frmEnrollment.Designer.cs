6aedb1bc231597f479429f05aec6298dbfa18ba6
7sluck <56097522+7sluck@users.noreply.github.com>
2019-10-10 21:46:51 -0500

    Update frmEnrollment.Designer.cs
    Changes tab order.

--- Group Project1/TinyCollege/frmEnrollment.Designer.cs
+++ Group Project1/TinyCollege/frmEnrollment.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace TinyCollege
+namespace TinyCollege
 {
     partial class frmEnrollment
     {
@@ -53,9 +53,11 @@
             this.groupBox1.Controls.Add(this.cmbStudent);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(50, 34);
+            this.groupBox1.Location = new System.Drawing.Point(33, 22);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(700, 382);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(467, 248);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Enrollment Data";
@@ -64,28 +66,31 @@
             // 
             this.lblSemester.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.lblSemester.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSemester.Location = new System.Drawing.Point(374, 201);
+            this.lblSemester.Location = new System.Drawing.Point(249, 131);
+            this.lblSemester.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblSemester.Name = "lblSemester";
-            this.lblSemester.Size = new System.Drawing.Size(277, 23);
+            this.lblSemester.Size = new System.Drawing.Size(185, 15);
             this.lblSemester.TabIndex = 7;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(90, 201);
+            this.label3.Location = new System.Drawing.Point(60, 131);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(123, 29);
+            this.label3.Size = new System.Drawing.Size(82, 20);
             this.label3.TabIndex = 6;
             this.label3.Text = "Semester:";
             // 
             // btnClose
             // 
             this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClose.Location = new System.Drawing.Point(395, 278);
+            this.btnClose.Location = new System.Drawing.Point(263, 181);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(189, 58);
-            this.btnClose.TabIndex = 5;
+            this.btnClose.Size = new System.Drawing.Size(126, 38);
+            this.btnClose.TabIndex = 3;
             this.btnClose.Text = "Cl&ose";
             this.btnClose.UseVisualStyleBackColor = true;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
@@ -95,10 +100,11 @@
             // enrollButton
             // 
             this.enrollButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.enrollButton.Location = new System.Drawing.Point(117, 278);
+            this.enrollButton.Location = new System.Drawing.Point(78, 181);
+            this.enrollButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.enrollButton.Name = "enrollButton";
-            this.enrollButton.Size = new System.Drawing.Size(189, 58);
-            this.enrollButton.TabIndex = 4;
+            this.enrollButton.Size = new System.Drawing.Size(126, 38);
+            this.enrollButton.TabIndex = 2;
             this.enrollButton.Text = "&Enroll";
             this.enrollButton.UseVisualStyleBackColor = true;
             this.enrollButton.Click += new System.EventHandler(this.EnrollButton_Click);
@@ -108,10 +114,11 @@
             // cmbCourse
             // 
             this.cmbCourse.FormattingEnabled = true;
-            this.cmbCourse.Location = new System.Drawing.Point(374, 142);
+            this.cmbCourse.Location = new System.Drawing.Point(249, 92);
+            this.cmbCourse.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cmbCourse.Name = "cmbCourse";
-            this.cmbCourse.Size = new System.Drawing.Size(277, 28);
-            this.cmbCourse.TabIndex = 3;
+            this.cmbCourse.Size = new System.Drawing.Size(186, 21);
+            this.cmbCourse.TabIndex = 1;
             this.cmbCourse.SelectedIndexChanged += new System.EventHandler(this.CmbCourse_SelectedIndexChanged);
             this.cmbCourse.MouseLeave += new System.EventHandler(this.CmbCourse_MouseLeave);
             this.cmbCourse.MouseHover += new System.EventHandler(this.CmbCourse_MouseHover);
@@ -119,10 +126,11 @@
             // cmbStudent
             // 
             this.cmbStudent.FormattingEnabled = true;
-            this.cmbStudent.Location = new System.Drawing.Point(374, 75);
+            this.cmbStudent.Location = new System.Drawing.Point(249, 49);
+            this.cmbStudent.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cmbStudent.Name = "cmbStudent";
-            this.cmbStudent.Size = new System.Drawing.Size(277, 28);
-            this.cmbStudent.TabIndex = 2;
+            this.cmbStudent.Size = new System.Drawing.Size(186, 21);
+            this.cmbStudent.TabIndex = 0;
             this.cmbStudent.SelectedIndexChanged += new System.EventHandler(this.StudentComboBox_SelectedIndexChanged);
             this.cmbStudent.MouseLeave += new System.EventHandler(this.CmbStudent_MouseLeave);
             this.cmbStudent.MouseHover += new System.EventHandler(this.CmbStudent_MouseHover);
@@ -131,9 +139,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(90, 142);
+            this.label2.Location = new System.Drawing.Point(60, 92);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(190, 29);
+            this.label2.Size = new System.Drawing.Size(126, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Select a Course:";
             // 
@@ -141,9 +150,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(90, 75);
+            this.label1.Location = new System.Drawing.Point(60, 49);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(194, 29);
+            this.label1.Size = new System.Drawing.Size(132, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Select a Student:";
             // 
@@ -152,24 +162,26 @@
             this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 427);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 270);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 9, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(533, 22);
             this.statusStrip1.TabIndex = 1;
             this.statusStrip1.Text = "statusStrip1";
             // 
             // toolStripStatusLabel1
             // 
             this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 15);
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
             // 
             // frmEnrollment
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 449);
+            this.ClientSize = new System.Drawing.Size(533, 292);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmEnrollment";
             this.Text = "Enroll a Student In a Course";
             this.groupBox1.ResumeLayout(false);
